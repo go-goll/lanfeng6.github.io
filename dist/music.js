64dfcf50779627cd5ac21133ee90688698fa5d9a
zlp <you@example.com944519712@qq.com>
2019-02-22 20:46:30 +0800

    Site updated: 2019-02-22 20:46:29

---
+++ dist/music.js
@@ -0,0 +1,11 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    mini: true,
+    autoplay: true,
+    audio: [{
+        name: 'kiss the rain',
+        artist: 'YIRUMA',
+        url: 'http://www.ytmp3.cn/down/46770.mp3',
+        cover: 'https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=3606890137,1028333492&fm=58&bpow=510&bpoh=382',
+    }]
+});
